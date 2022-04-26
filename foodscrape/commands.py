0285ef4f3c9e8732242bce268c12fdbf63d2ce31
Harrison Ayan <harrisonayan@gmail.com>
2022-04-25 23:04:59 -0400

    add test command

--- foodscrape/commands.py
+++ foodscrape/commands.py
@@ -1,7 +1,9 @@
 import os
-import unittest
 
 import click
+
+# import unittest
+
 
 FILE_PATH = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.join(FILE_PATH, os.pardir)
@@ -10,14 +12,7 @@ TEST_PATH = os.path.join(PROJECT_ROOT, "tests")
 
 @click.command()
 def test():
-    tests = unittest.TestLoader().discover(TEST_PATH, pattern="test_*.py")
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
-
-
-# pytest --cov=. tests/ --cov-report xml:cov.xml
+    os.system("pytest --cov=. tests/ --cov-report xml:cov.xml")
 
 
 def hello():
