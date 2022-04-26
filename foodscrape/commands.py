39d6c848bb2db6cd00d08752339de945f6f66d7d
Harrison Ayan <harrisonayan@gmail.com>
2022-04-25 23:05:26 -0400

    remove

--- foodscrape/commands.py
+++ foodscrape/commands.py
@@ -2,9 +2,6 @@ import os
 
 import click
 
-# import unittest
-
-
 FILE_PATH = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.join(FILE_PATH, os.pardir)
 TEST_PATH = os.path.join(PROJECT_ROOT, "tests")
